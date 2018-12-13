8ad54e6b1aa2e949916810e1ff60c9f5c79ccb46
TheFamousDirector <FamousDirector@users.noreply.github.com>
2018-12-13 15:24:02 -0400

    completed basic physical interface

--- main.py
+++ main.py
@@ -2,14 +2,17 @@ import RPi.GPIO as GPIO
 from luma.core.interface.serial import i2c
 from luma.core.render import canvas
 from luma.oled.device import ssd1306, ssd1325, ssd1331, sh1106
+import print_comic
 
 
 class ComicPrinter:
 
     def print_comic_callback(self, channel):
         GPIO.output(5, GPIO.LOW)
-        print "print comic"
+        self.update_screen(input='PRINTING...')
+        print_comic.print_comic(self.comic_lookup_array[self.comic_index])
         GPIO.output(5, GPIO.HIGH)
+        self.update_screen()
 
     def change_comic_callback(self, channel):
         clk_state = GPIO.input(self.clk_pin)
@@ -37,11 +40,15 @@ class ComicPrinter:
         else:
             self.comic_index = len(self.comic_lookup_array)-1
 
+    def update_screen(self, input=None):
+        if input is None:
+            display_string = 'Select a comic: \n' + self.comic_name_array[self.comic_index]
+        else:
+            display_string = input
 
-    def update_screen(self):
         with canvas(self.screen) as draw:
             draw.rectangle(self.screen.bounding_box, outline="white", fill="black")
-            draw.text((5, 5), self.comic_name_array[self.comic_index], fill="white")
+            draw.text((5, 5), display_string, fill="white")
 
 
     def __init__(self):
@@ -63,7 +70,7 @@ class ComicPrinter:
         GPIO.setup(self.dt_pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # encoder right - pin 36
         GPIO.setup(26, GPIO.IN, pull_up_down=GPIO.PUD_UP)  # push button - pin 32
 
-        GPIO.add_event_detect(self.clk_pin, GPIO.FALLING, callback=self.change_comic_callback, bouncetime=5)
+        GPIO.add_event_detect(self.clk_pin, GPIO.FALLING, callback=self.change_comic_callback, bouncetime=50)
         GPIO.add_event_detect(26, GPIO.FALLING, callback=self.print_comic_callback, bouncetime=300)
 
         # outputs
